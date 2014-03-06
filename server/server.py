cf469f2880e03055ff9e146ebd6fd95136065998
Spencer Nelson <spencer@twitch.tv>
2014-03-05 18:33:34 -0800

    Add an index describing the server's resources

--- server/server.py
+++ server/server.py
@@ -5,6 +5,40 @@ app = Flask('wine-db-backend')
 import database
 
 
+@app.route('/')
+def index():
+    docs = {
+        'title': 'wine-db',
+        'description': 'This is a simple REST server for a wine database app.',
+        'endpoints': [{
+            'name': 'Lookup a wine',
+            'method': 'GET',
+            'url': '/api/wine/<int:id>',
+            'description': 'Look up a wine',
+            'returns': 'An indvidual wine with the given ID as a JSON-encoded string',
+        }, {
+            'name': 'Update a wine',
+            'method': 'POST',
+            'url': '/api/wine/<int:id>',
+            'description': 'Updates a wine. Attach POST data as JSON, and set content-type as application/json.',
+            'returns': 'ID of the wine as a JSON string like {"id": <id>}'
+        }, {
+            'name': 'Insert a wine',
+            'method': 'PUT',
+            'url': '/api/wine',
+            'description': 'Inserts a wine. Attach data as JSON, and set content-type as application/json.',
+            'returns': 'ID of the inserted wine as a JSON string like {"id": <id>}'
+        }, {
+            'name': 'Fetch the full list of wines',
+            'method': 'GET',
+            'url': '/api/wine/list',
+            'description': 'List all wines',
+            'returns': 'A JSON-encoded list of wine objects'
+        }]
+    }
+
+    return jsonify(**docs)
+
 @app.route('/api/wine/<int:id>', methods=['GET'])
 def get_wine(id):
     wine = database.get_wine(id)
