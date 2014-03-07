fdc1c662aeabc443cd90d8f650fe0916688e8de6
aliyanno <aliyamarder@gmail.com>
2014-03-07 13:58:01 -0800

    Synchronizing 2

--- static/js/wine-db.js
+++ static/js/wine-db.js
@@ -1,2 +1,17 @@
 var wineApp = angular.module('wineApp', []);
 
+wineApp.controller('wineCtrl', ['$scope', function($scope) {
+
+}]);
+
+
+wineApp.factory('availableWines', ['$http', function($http) {
+		return {
+			getWine: function(id) {
+				return $http({method: 'GET', url('/api/wine/' + id)})
+			};
+			updateWine: function(id) {
+				return $http({method: })
+			}
+		}
+}])
